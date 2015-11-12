3802cba4b327df8a7605c6d9cd079b463436dbeb
mushoe <alphaneo1@MacBook-Pro.local>
2015-11-11 19:46:36 -0800

    Merged both code bases, both modules run, some minor syntax errors to be fixed to display the radio button, db integration still left to do with user registration.

--- target/scala-2.11/twirl/main/views/html/rate.template.scala
+++ target/scala-2.11/twirl/main/views/html/rate.template.scala
@@ -28,13 +28,13 @@ import helper._
 
 Seq[Any](format.raw/*3.85*/("""
 """),format.raw/*5.1*/("""
+"""),_display_(/*6.2*/base(title)/*6.13*/ {_display_(Seq[Any](format.raw/*6.15*/("""	"""),format.raw/*6.63*/("""
 
-"""),_display_(/*7.2*/base(title)/*7.13*/ {_display_(Seq[Any](format.raw/*7.15*/("""	"""),format.raw/*7.63*/("""
-
-    """),_display_(/*9.6*/form(action = routes.Recommender.results(), 'id -> "ratingsForm")/*9.71*/ {_display_(Seq[Any](format.raw/*9.73*/("""
-        """),_display_(/*10.10*/for((movie, index) <- movies.zipWithIndex) yield /*10.52*/ {_display_(Seq[Any](format.raw/*10.54*/("""
-            
-        """)))}),format.raw/*12.10*/("""
+    """),_display_(/*8.6*/form(action = routes.Recommender.results(), 'id -> "ratingsForm")/*8.71*/ {_display_(Seq[Any](format.raw/*8.73*/("""
+        """),_display_(/*9.10*/for((movie, index) <- movies.zipWithIndex) yield /*9.52*/ {_display_(Seq[Any](format.raw/*9.54*/("""
+            """),_display_(/*10.14*/movie),format.raw/*10.19*/("""
+        """)))}),format.raw/*11.10*/("""
+        
         """),format.raw/*13.9*/("""<input type="submit" class="tenratings" value="Get Recommendations">         
     """)))}),format.raw/*14.6*/("""
     """),format.raw/*15.43*/("""
@@ -60,11 +60,11 @@ Seq[Any](format.raw/*3.85*/("""
 object rate extends rate_Scope0.rate_Scope1.rate
               /*
                   -- GENERATED --
-                  DATE: Wed Nov 11 18:43:53 PST 2015
+                  DATE: Wed Nov 11 19:43:35 PST 2015
                   SOURCE: /Users/alphaneo1/movie/cs490_movie_rec/app/views/rate.scala.html
-                  HASH: cf13204ad583d846272c38eb98a87830c40e122f
-                  MATRIX: 633->39|826->122|853->140|881->143|900->154|939->156|967->204|999->211|1072->276|1111->278|1148->288|1206->330|1246->332|1300->355|1336->364|1449->447|1482->490|1514->492
-                  LINES: 24->3|29->3|30->5|32->7|32->7|32->7|32->7|34->9|34->9|34->9|35->10|35->10|35->10|37->12|38->13|39->14|40->15|41->16
+                  HASH: da15e03abfa319461a37b9f5cdefecb7b42dff65
+                  MATRIX: 633->39|826->122|853->140|880->142|899->153|938->155|966->203|998->210|1071->275|1110->277|1146->287|1203->329|1242->331|1283->345|1309->350|1350->360|1395->378|1508->461|1541->504|1573->506
+                  LINES: 24->3|29->3|30->5|31->6|31->6|31->6|31->6|33->8|33->8|33->8|34->9|34->9|34->9|35->10|35->10|36->11|38->13|39->14|40->15|41->16
                   -- GENERATED --
               */
           
